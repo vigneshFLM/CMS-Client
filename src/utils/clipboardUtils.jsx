c219f6f9beba9bd718de6ec5be07c7c16d6936d0
Vignesh <112454916+VigneshEmbari@users.noreply.github.com>
2025-06-05 13:22:12 +0530

    adding logs for copying credential

--- src/utils/clipboardUtils.jsx
+++ src/utils/clipboardUtils.jsx
@@ -1,13 +1,16 @@
 export const handleCopy = (text, setCopiedField, field) => {
-  console.log("Attempting to copy:", text);  // Log text to be copied
-  navigator.clipboard.writeText(text).then(
-    () => {
-      console.log("Text copied to clipboard!");
-      setCopiedField(field);
-      setTimeout(() => setCopiedField(""), 1500);
-    },
-    (err) => {
-      console.error("Error copying text: ", err);  // Handle potential errors
-    }
-  );
+  if (navigator.clipboard) {
+    navigator.clipboard.writeText(text).then(
+      () => {
+        console.log("Text copied to clipboard!");
+        setCopiedField(field);
+        setTimeout(() => setCopiedField(""), 1500);
+      },
+      (err) => {
+        console.error("Error copying text: ", err);
+      }
+    );
+  } else {
+    console.error("Clipboard API is not available");
+  }
 };
